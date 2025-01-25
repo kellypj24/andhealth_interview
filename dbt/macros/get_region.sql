7c22cd07b69413707e501d0444e9e08f4036fee2
PJ Kellym <kellypj24@gmail.com>
2025-01-25 10:35:54 -0500

    add macro to simplify region logic

---
+++ dbt/macros/get_region.sql
@@ -0,0 +1,19 @@
+{# macros/get_region.sql #}
+{% macro get_region(state_column) %}
+
+{% set state_regions = {
+    'Northeast': ['CT', 'ME', 'MA', 'NH', 'RI', 'VT', 'NJ', 'NY', 'PA'],
+    'Midwest': ['IL', 'IN', 'MI', 'OH', 'WI', 'IA', 'KS', 'MN', 'MO', 'NE', 'ND', 'SD'],
+    'South': ['DE', 'FL', 'GA', 'MD', 'NC', 'SC', 'VA', 'DC', 'WV', 'AL', 'KY', 'MS', 'TN', 'AR', 'LA', 'OK', 'TX'],
+    'West': ['AZ', 'CO', 'ID', 'MT', 'NV', 'NM', 'UT', 'WY', 'AK', 'CA', 'HI', 'OR', 'WA']
+} %}
+
+case {{ state_column }}
+    {% for region, states in state_regions.items() %}
+        {% for state in states %}
+    when '{{ state }}' then '{{ region }}'
+        {% endfor %}
+    {% endfor %}
+end
+
+{% endmacro %}
